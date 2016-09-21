a6988fba84dd82769405df0daae6fabcbf45ad4b
Kahou <creator.kahou@gmail.com>
2016-09-21 18:04:41 +0800

    Add SmtpServerEnum

---
+++ SmtpServerEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace System.Net.Mail
+{
+    /// <summary>
+    /// 指定 SMTP 伺服器。
+    /// </summary>
+    public enum SmtpServer
+    {
+        [SmtpServer("smtp.gmail.com", 587)]
+        Gmail = 1,
+
+        [SmtpServer("smtp-mail.outlook.com", 587)]
+        Outlook = 2,
+    }
+}
