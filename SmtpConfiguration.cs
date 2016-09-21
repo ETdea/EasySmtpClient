21d377d8f7841c4c559af262432e8e83db7e2495
Kahou <creator.kahou@gmail.com>
2016-09-21 20:13:00 +0800

    Add SmtpConfiguration

---
+++ SmtpConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+using System.Net.Configuration;
+using System.Web;
+using System.Web.Configuration;
+
+namespace System.Net.Mail
+{
+    public class SmtpConfiguration
+    {
+        private System.Configuration.Configuration config;
+        private MailSettingsSectionGroup sectionGroup;
+
+        public SmtpSection Settings
+        {
+            get
+            {
+                if (sectionGroup == null)
+                {
+                    sectionGroup = config.GetSectionGroup("system.net/mailSettings") as MailSettingsSectionGroup;
+                }
+
+                return sectionGroup.Smtp;
+            }
+        }
+
+        public SmtpConfiguration(System.Configuration.Configuration config = null)
+        {
+            if (config == null)
+            {
+                config = WebConfigurationManager.OpenWebConfiguration(HttpContext.Current.Request.ApplicationPath);
+            }
+
+            this.config = config;
+        }
+
+        public void save()
+        {
+            config.Save();
+        }
+    }
+}
